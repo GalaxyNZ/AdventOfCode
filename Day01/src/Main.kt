c041e8f4861fad0f4a58087d7676b9257bff3e37
Luke C <62201054+GalaxyNZ@users.noreply.github.com>
2022-12-01 22:37:26 +1300

    Day 1 Part 2 + comments

--- Day01/src/Main.kt
+++ Day01/src/Main.kt
@@ -9,7 +9,6 @@ object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         println(part1().last())
-
         println(part2())
     }
 
@@ -17,24 +16,28 @@ object Main {
         // Set up scanner
         val scanner = Scanner(File("Day01/puzzle_input"))
 
+        // Iterate through each line
         while (scanner.hasNext()) {
             val line = scanner.nextLine()
-            // if it is a number
-            if (!line.isNullOrBlank()) {
+
+            if (!line.isNullOrBlank()) { // if it is a number
                 calories += line.toInt()
-            } else {
+            } else { // if it is a blank line
                 calories_list += calories
                 calories = 0
             }
         }
 
+        // sort list and return
         calories_list.sort()
         return calories_list
     }
 
     fun part2(): Int {
+        // Reverse the list
         calories_list.reverse()
 
+        // Add the first 3 and return
         return calories_list[0] + calories_list[1] + calories_list[2]
     }
 }
