6b8c41b0523f9d5bbfd6d60b26f8a84061b837b4
Luke C <62201054+GalaxyNZ@users.noreply.github.com>
2022-12-01 22:21:49 +1300

    Day 1 Part 1

--- Day01/src/Main.kt
+++ Day01/src/Main.kt
@@ -1,8 +1,28 @@
+import java.io.File
+import java.util.*
+
 object Main {
+
+    var calories_list = arrayOf<Int>()
+    var calories = 0
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Hello world!")
-        println(args.toString())
+        // Set up scanner
+        val scanner = Scanner(File("Day01/puzzle_input"))
+
+        while (scanner.hasNext()) {
+            val line = scanner.nextLine()
+            // if it is a number
+            if (!line.isNullOrBlank()) {
+                calories += line.toInt()
+            } else {
+                calories_list += calories
+                calories = 0
+            }
+        }
+
+        calories_list.sort()
+        println(calories_list.last())
     }
 }
