7973a3520c9cabbbb0cd6c63a220c725e0bc062d
Luke Catherall <luke.catherall@kiwibank.co.nz>
2022-12-05 11:20:04 +1300

    Day 4 Part 2

--- Day04/src/Day4.kt
+++ Day04/src/Day4.kt
@@ -7,6 +7,8 @@ object Day4 {
     @JvmStatic
     fun main(args: Array<String>) {
         part1()
+
+        part2()
     }
 
     private fun part1() {
@@ -22,6 +24,25 @@ object Day4 {
             if (
                 (sectionsInt[0] <= sectionsInt[2] && sectionsInt[1] >= sectionsInt[3]) ||
                 (sectionsInt[0] >= sectionsInt[2] && sectionsInt[1] <= sectionsInt[3])
+            ) {
+                sum++
+            }
+        }
+        println(sum)
+    }
+
+    private fun part2() {
+        var sum = 0
+        for (line in lines) {
+            val sectionsStr = line.split(",", "-")
+            val sections = mutableListOf<Int>()
+
+            for (section in sectionsStr) {
+                sections.add(section.toInt())
+            }
+
+            if (
+                !(sections[0] > sections[3] || sections[2] > sections[1])
             ) {
                 sum++
             }
